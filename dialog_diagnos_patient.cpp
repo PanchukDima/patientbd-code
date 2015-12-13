6ad193968daf8f43d8faa9626f438231f4c622cd
panch-dima <panchuk6@gmail.com>
2015-12-13 12:17:01 +0300

    edit form diagnos patient

--- dialog_diagnos_patient.cpp
+++ dialog_diagnos_patient.cpp
@@ -12,3 +12,26 @@ Dialog_diagnos_patient::~Dialog_diagnos_patient()
 {
     delete ui;
 }
+
+void Dialog_diagnos_patient::setParam(int param, QString id, QString staff_id)
+{
+    global_param =param;
+    global_id = id;
+    global_staff_id =staff_id;
+    switch (param) {
+    case 0:
+        //add diagnos
+        break;
+    case 1:
+        //edit duiagnos
+        break;
+    }
+}
+void Dialog_diagnos_patient::get_data()
+{
+
+}
+void Dialog_diagnos_patient::send_data()
+{
+
+}
--- dialog_diagnos_patient.h
+++ dialog_diagnos_patient.h
@@ -14,9 +14,17 @@ class Dialog_diagnos_patient : public QDialog
 public:
     explicit Dialog_diagnos_patient(QWidget *parent = 0);
     ~Dialog_diagnos_patient();
+    int global_param;
+    QString global_id;
+    QString global_staff_id;
 
 private:
     Ui::Dialog_diagnos_patient *ui;
+public slots:
+    void setParam(int param, QString id, QString staff_id);
+    void get_data();
+    void send_data();
 };
+
 
 #endif // DIALOG_DIAGNOS_PATIENT_H
